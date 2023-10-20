7a392237d9d16d68ead25ebb23b47108ed69d4bb
Ramzil <ibragimovramzil53@gmail.com>
2023-10-20 09:34:48 +0500

    task 2.1

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,9 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    public function bbs() {
+        return $this->hasMany(Bb::class);
+        }
     /**
      * The attributes that are mass assignable.
      *
