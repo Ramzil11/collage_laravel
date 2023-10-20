a1c411aebba5adab0059f498795de1b5e8195592
Ramzil <ibragimovramzil53@gmail.com>
2023-10-20 09:58:51 +0500

    task 2.2

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,11 @@ use App\Http\Controllers\BbsController;
 */
 
 Route::get('/', [BbsController::class, 'index'])->name('index');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class,
+ 'index'])
+ ->name('home');
 Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
